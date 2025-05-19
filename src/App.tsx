f6b11e6ec4b0c7700f2b4d85a3dde3fe06b97a6f
Adam-Elmi <Adamcade123@gmail.com>
2025-05-19 23:57:21 +0300

    Modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css';
+import { CircleLoading } from './icons/LoadingIcons';
 import Button from './ui/Button';
 function App() {
  return (
-  <Button ptop={20} textColor="#ddd" fontSize="1rem" border="2px solid gray" radius={10} background="#333"></Button>
+  <>
+  <Button textColor="#ddd" fontSize="1rem" textCase='capital' disabled={true} onClick={() => console.log("Hello Adam")}>hello adam</Button>
+  <Button icon={<CircleLoading/>} textColor="#ddd" fontSize="1rem" textCase='capital' onClick={() => console.log("Hello Adam")} allow_repeat={true} repeat={3} allow_timer={true} timer={5000}>hello adam</Button>
+  </>
  )
 }
 
