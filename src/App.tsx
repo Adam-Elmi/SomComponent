d6cc33be892d2ff9bed396155a720ecf46a2e9b4
Adam-Elmi <Adamcade123@gmail.com>
2025-05-20 09:38:03 +0300

    Modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import './App.css';
-import { CircleLoading } from './icons/LoadingIcons';
-import Button from './ui/Button';
+import "./App.css";
+import ArrowIcon from "./icons/ArrowIcon";
+import Button from "./ui/Button";
 function App() {
- return (
-  <>
-  <Button textColor="#ddd" fontSize="1rem" textCase='capital' disabled={true} onClick={() => console.log("Hello Adam")}>hello adam</Button>
-  <Button icon={<CircleLoading/>} textColor="#ddd" fontSize="1rem" textCase='capital' onClick={() => console.log("Hello Adam")} allow_repeat={true} repeat={1} allow_timer={true} timer={2000}>hello adam</Button>
-  </>
- )
+  return (
+    <>
+      <Button
+        textColor="#ddd"
+        fontSize="1rem"
+        textCase="capital"
+        disabled={true}
+        onClick={() => console.log("Hello Adam")}
+      >
+        hello adam
+      </Button>
+      <Button
+        icon={<ArrowIcon />}
+        icon_position="right"
+        textColor="#ddd"
+        fontSize="1rem"
+        textCase="capital"
+        onClick={() => console.log("Hello Adam")}
+        allow_repeat={true}
+        repeat={1}
+        allow_timer={true}
+        timer={2000}
+        disabled_text="Sending! Please wait"
+        style={{height: "50px"}}
+      >
+        Send message
+      </Button>
+    </>
+  );
 }
 
-export default App
+export default App;
