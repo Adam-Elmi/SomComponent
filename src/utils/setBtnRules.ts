afd1c41a1d5ceaf9c7e5c3f2bab26987c240c9ba
Adam-Elmi <Adamcade123@gmail.com>
2025-05-20 08:52:04 +0300

    Modified setBtnRules.ts

--- src/utils/setBtnRules.ts
+++ src/utils/setBtnRules.ts
@@ -18,25 +18,30 @@ export default function setBtnRules({
   timer,
 }: Props) {
   let count = 0;
+
   if (btn) {
     const element = btn as HTMLButtonElement;
+    const content = element.innerHTML;
     if (allow_repeat) {
       const handle_repeat = () => {
         count++;
         if (repeat === count) {
           element.disabled = true;
           isDisabled(element, opacity_level);
-          if (allow_timer) {
-            setTimeout(() => {
-              element.disabled = false;
-              isDisabled(element, opacity_level);
+          count = 0;
+          element.textContent = "Button disabled";
+            
+            if (allow_timer) {
+                setTimeout(() => {
+                    element.disabled = false;
+                    isDisabled(element, opacity_level);
+                     element.innerHTML = content;
             }, timer);
-          }
-          element.removeEventListener("click", handle_repeat);
         }
-        console.log(count);
-      };
-      element.addEventListener("click", handle_repeat);
+    }
+    console.log(count);
+};
+element.addEventListener("click", handle_repeat);
     }
   }
 }
