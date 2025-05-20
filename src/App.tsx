16441f51230b458bb5668c09af18d51441f671a5
Adam-Elmi <Adamcade123@gmail.com>
2025-05-20 08:51:58 +0300

    Modified App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
  return (
   <>
   <Button textColor="#ddd" fontSize="1rem" textCase='capital' disabled={true} onClick={() => console.log("Hello Adam")}>hello adam</Button>
-  <Button icon={<CircleLoading/>} textColor="#ddd" fontSize="1rem" textCase='capital' onClick={() => console.log("Hello Adam")} allow_repeat={true} repeat={3} allow_timer={true} timer={5000}>hello adam</Button>
+  <Button icon={<CircleLoading/>} textColor="#ddd" fontSize="1rem" textCase='capital' onClick={() => console.log("Hello Adam")} allow_repeat={true} repeat={1} allow_timer={true} timer={2000}>hello adam</Button>
   </>
  )
 }
