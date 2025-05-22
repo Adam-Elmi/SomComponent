f2a61e9811360bacd3c3153c99b08f643d737e33
Adam-Elmi <Adamcade123@gmail.com>
2025-05-22 10:37:43 +0300

    Modified App.tsx to update application logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import ArrowIcon from "./icons/ArrowIcon";
 import SmartButton from "./components/Button";
+import SmartHeader from "./components/Header";
+import SmartLink from "./components/Link";
 function App() {
   return (
     <>
-      <SmartButton
+      {/* <SmartButton
         textCase="capital"
         disabled={true}
         onClick={() => console.log("Hello Adam")}
@@ -19,6 +21,12 @@ function App() {
         rules={{wait: 1, maxClicks: 2}}
         disabled_text="Sending! Please wait"
         style={{ height: "50px" }}
+      /> */}
+      {/* <SmartHeader/> */}
+      <SmartLink
+        text="Learn more what Node.js"
+        icon_position="right"
+        link_title="SomCheat"
       />
     </>
   );
