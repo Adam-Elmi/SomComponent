b9f572e8d4f5ec5f78f732e32128c24cd0d0dba4
Adam-Elmi <Adamcade123@gmail.com>
2025-05-24 06:51:31 +0300

    Modified App.tsx to integrate recent updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,47 @@
 import "./App.css";
 import ArrowIcon from "./icons/ArrowIcon";
 import SmartButton from "./components/Button";
-import SmartHeader from "./components/Header";
-import SmartLink from "./components/Link";
+import { useEffect, useState } from "react";
+import { BubbleLoading } from "./icons/LoadingIcons";
+// import SmartHeader from "./components/Header";
+// import SmartLink from "./components/Link";
 function App() {
+  const [fetchData, setFetchData] = useState<[]>([]);
+  const [checkState, setCheckState] = useState<boolean>(false);
+  useEffect(() => {
+    function fetchUser() {
+    fetch("https://api.github.com/users/Adam-Edlmi")
+      .then(res => {
+        if(res.ok) {
+          setCheckState(false);
+          return res.json();
+        } else {
+          setCheckState(true);
+        }
+      })
+      .then(data => {
+        console.log(data);
+          setFetchData(data);
+      })
+    }
+    const btn = document.getElementById("btn");
+    if(btn) {
+      btn.addEventListener("click", fetchUser);
+    }
+  }, [fetchData, checkState])
+  console.log(checkState)
   return (
     <>
-      {/* <SmartButton
+      <SmartButton
+      id="btn"
         textCase="capital"
-        disabled={true}
+        disabled={checkState}
+        disabled_text="Fetch data failed!"
         onClick={() => console.log("Hello Adam")}
       />
 
       <SmartButton
+      disabled_icon={<BubbleLoading/>}
         label="Send me"
         icon={<ArrowIcon />}
         icon_position="right"
@@ -21,13 +50,14 @@ function App() {
         rules={{wait: 1, maxClicks: 2}}
         disabled_text="Sending! Please wait"
         style={{ height: "50px" }}
-      /> */}
+      />
       {/* <SmartHeader/> */}
-      <SmartLink
+      {/* <SmartLink
         text="Learn more what Node.js"
         icon_position="right"
         link_title="SomCheat"
-      />
+        variant="descriptive"
+      /> */}
     </>
   );
 }
