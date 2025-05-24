680d318ca7a2dfd9a5cae394f9abd02d8184d9cf
Adam-Elmi <Adamcade123@gmail.com>
2025-05-24 06:51:34 +0300

    Modified Button index.tsx to enhance button logic

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import setCase from "../../utils/setCase";
 import type { SmartButtonProps } from "./types";
 import { default_styles } from "./styles";
@@ -10,33 +11,49 @@ const SmartButton: React.FC<SmartButtonProps> = ({
   textCase,
   disable_opacity = "0.5",
   disabled_text,
+  disabled_icon,
   rules = {},
   onClick,
   ...props
 }) => {
-  const { handleClick, canClick, clickCount } = useButtonRules(rules);
-
+  const { handleClick, canClick, clickCount, checkDisability } = useButtonRules(rules);
+  const smartBtnRef = useRef(null);
+  const [isDisable, setIsDisable] = useState<boolean>(false);
+  const [updateText, setUpdateText] = useState<string | undefined>(label);
+  const [updateIcon, setUpdateIcon] = useState<React.ReactElement<HTMLImageElement | SVGElement> | undefined>(icon);
+  
   const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     if (!canClick()) return;
 
     handleClick();
     if (onClick) onClick(e);
   };
+  useEffect(() => {
+    const btn = smartBtnRef.current;
+ 
+    if (btn) {
+      setUpdateText(isDisable ? disabled_text || "" : !canClick() ? disabled_text || "" : label ? setCase(label, textCase) : "Clicked " + clickCount);
+      setUpdateIcon(isDisable ? disabled_icon : !canClick() ? disabled_icon : icon);
+
+      setIsDisable(checkDisability(btn));
+    }
+  }, [isDisable, canClick, checkDisability, disabled_text, updateText, clickCount, label, textCase, disabled_icon, icon]);
   return (
     <button
+      ref={smartBtnRef}
       onClick={handleButtonClick}
-      disabled={!canClick()}
+      disabled={isDisable}
       {...props}
       style={{
         ...(default_styles || {}),
-        opacity: !canClick() ? disable_opacity : 1,
-        cursor: !canClick() ? "not-allowed" : "pointer",
+        opacity: (isDisable ? disable_opacity : !canClick() ? disable_opacity : 1),
+        cursor: (isDisable ? "not-allowed" : !canClick() ? "not-allowed" : "pointer"),
         ...(props.style || {}),
       }}
     >
-      {icon_position === "left" ? icon : null}
-      {label ? setCase(label, textCase) : "Clicked " + clickCount}
-      {icon_position === "right" ? icon : null}
+      {icon_position === "left" ? updateIcon : null}
+      {updateText}
+      {icon_position === "right" ? updateIcon : null}
     </button>
   );
 };
