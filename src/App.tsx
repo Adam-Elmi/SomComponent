2fde0cd29995ab8c11fb9a87b0109ae620ed8d46
Adam-Elmi <Adamcade123@gmail.com>
2025-05-24 10:57:29 +0300

    Modified App.tsx to reflect latest component integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,60 @@
 import "./App.css";
-import ArrowIcon from "./icons/ArrowIcon";
+// import ArrowIcon from "./icons/ArrowIcon";
 import SmartButton from "./components/Button";
 import { useEffect, useState } from "react";
-import { BubbleLoading } from "./icons/LoadingIcons";
 // import SmartHeader from "./components/Header";
-// import SmartLink from "./components/Link";
+import SmartLink from "./components/Link";
 function App() {
   const [fetchData, setFetchData] = useState<[]>([]);
   const [checkState, setCheckState] = useState<boolean>(false);
   useEffect(() => {
     function fetchUser() {
-    fetch("https://api.github.com/users/Adam-Edlmi")
-      .then(res => {
-        if(res.ok) {
-          setCheckState(false);
-          return res.json();
-        } else {
-          setCheckState(true);
-        }
-      })
-      .then(data => {
-        console.log(data);
+      fetch("https://api.github.com/users/Adam-Edlmi")
+        .then((res) => {
+          if (res.ok) {
+            setCheckState(false);
+            return res.json();
+          } else {
+            setCheckState(true);
+          }
+        })
+        .then((data) => {
+          console.log(data);
           setFetchData(data);
-      })
+        });
     }
     const btn = document.getElementById("btn");
-    if(btn) {
+    if (btn) {
       btn.addEventListener("click", fetchUser);
     }
-  }, [fetchData, checkState])
-  console.log(checkState)
+  }, [fetchData, checkState]);
+  console.log(checkState);
   return (
     <>
       <SmartButton
-      id="btn"
+        id="btn"
         textCase="capital"
         disabled={checkState}
-        disabled_text="Fetch data failed!"
+        disabled_text="Failed to fetch data!"
         onClick={() => console.log("Hello Adam")}
       />
 
       <SmartButton
-      disabled_icon={<BubbleLoading/>}
-        label="Send me"
-        icon={<ArrowIcon />}
+        variant="multi"
+        label="My projects"
         icon_position="right"
         textCase="capital"
         onClick={() => console.log("Hello Adam")}
-        rules={{wait: 1, maxClicks: 2}}
-        disabled_text="Sending! Please wait"
-        style={{ height: "50px" }}
+        
       />
       {/* <SmartHeader/> */}
-      {/* <SmartLink
+      <SmartLink
         text="Learn more what Node.js"
         icon_position="right"
         link_title="SomCheat"
         variant="descriptive"
-      /> */}
+        show_arrow={true}
+      />
     </>
   );
 }
