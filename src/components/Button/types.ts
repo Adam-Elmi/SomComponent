f8ccc8eb82f35607267687a3bf22820406e57ba5
Adam-Elmi <Adamcade123@gmail.com>
2025-05-24 06:51:37 +0300

    Modified Button types.ts to update type definitions

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,10 +1,11 @@
 export type SmartButtonProps  = {
-  icon?: React.ReactElement;
+  icon?: React.ReactElement<HTMLImageElement | SVGElement>;
   icon_position?: "left" | "right";
   label?: string;
   textCase?: string | undefined;
   disable_opacity?: string;
   disabled_text?: string;
+  disabled_icon?: React.ReactElement<HTMLImageElement | SVGElement>
   rules?: {
     maxClicks?: number;
     wait?: number;
