e3a9c51906e7be83c5bec45f35339e6970453d8b
Adam-Elmi <Adamcade123@gmail.com>
2025-05-24 10:57:42 +0300

    Modified Link variants.tsx to reflect recent UI changes

--- src/components/Link/variants.tsx
+++ src/components/Link/variants.tsx
@@ -1,29 +1,9 @@
 import type { SmartLinkProps } from "./types";
-import {
-  default_styles,
-  icon_default_styles,
-  descriptive_styles,
-} from "./styles";
-import ArrowIcon from "../../icons/ArrowIcon";
+import { DefaultLink } from "./Variants/Default";
+import DescriptiveLink from "./Variants/Descriptive";
 
-const icon_value = ({
-  is_outside,
-  show_arrow,
-  icon_style,
-  icon,
-}: SmartLinkProps) => (
-  <span
-    style={{
-      ...(is_outside && show_arrow
-        ? { ...icon_default_styles, transform: "rotate(-45deg)", ...icon_style }
-        : { ...icon_default_styles, ...icon_style }),
-    }}
-  >
-    {show_arrow ? <ArrowIcon /> : icon}
-  </span>
-);
-
-export const DefaultLink: React.FC<SmartLinkProps> = ({
+const Variants: React.FC<SmartLinkProps> = ({
+  variant,
   path,
   text,
   icon,
@@ -31,64 +11,47 @@ export const DefaultLink: React.FC<SmartLinkProps> = ({
   is_outside,
   show_arrow = false,
   icon_style,
-  variant,
-  ...props
-}) => {
-  const getIcon = icon_value({ is_outside, show_arrow, icon_style, icon });
-  return (
-    <a
-      href={path}
-      target={is_outside ? "_blank" : "_parent"}
-      style={{ ...(default_styles || {}), ...(props.style || {}) }}
-      {...props}
-    >
-      {icon_position === "left" ? getIcon : null}
-      {text}
-      {icon_position === "right" ? getIcon : null}
-    </a>
-  );
-};
-
-export const DescriptiveLink: React.FC<SmartLinkProps> = ({
-  path,
-  text,
-  icon,
-  is_outside,
-  show_arrow = false,
-  icon_style,
-  link_title,
-  variant,
   descriptive_style,
   descriptive_title,
+  link_title,
   ...props
 }) => {
-  const getIcon = icon_value({ is_outside, show_arrow, icon_style, icon });
-  return (
-    <a
-      href={path}
-      target={is_outside ? "_blank" : "_parent"}
-      style={{
-        ...(descriptive_styles || {}),
-        ...(props.style || {}),
-        ...(descriptive_style || {}),
-      }}
-      {...props}
-    >
-      <span
-        style={{
-          borderRadius: "100px",
-          padding: "6px 12px",
-          backgroundColor: "#00dc82",
-          color: "#000",
-          fontFamily: "SharedTech",
-          ...(descriptive_title || {}),
-          ...(props.style || {}),
+  const variantLinks = {
+    default: (
+      <DefaultLink
+        {...{
+          path,
+          text,
+          icon,
+          icon_position,
+          is_outside,
+          show_arrow,
+          icon_style,
+          ...props,
         }}
-      >
-        {link_title}
-      </span>
-      {text}
-      {getIcon}
-    </a>
-  );
+      />
+    ),
+    descriptive: (
+      <DescriptiveLink {...{
+        path,
+        text,
+        icon,
+        is_outside,
+        show_arrow,
+        icon_style,
+        link_title,
+        descriptive_style,
+        descriptive_title,
+        ...props
+      }} />
+    )
+  };
+  switch (variant) {
+    case "descriptive":
+      return variantLinks.descriptive;
+    default:
+      return variantLinks.default;
+  }
 };
+
+export default Variants;
